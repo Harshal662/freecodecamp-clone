09f184f3f0606828c5c1d92ada0034284c57c2d4
Harshal <abakharshal@gmail.com>
2022-10-18 23:31:00 +0530

    deploy2

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ passport.use(new LocalStrategy(User.authenticate()))
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "/auth/google/secrets"
+    callbackURL: "/auth/google/callback"
 },
     async function (accessToken, refreshToken, profile, cb) {
         await User.findOrCreate(
